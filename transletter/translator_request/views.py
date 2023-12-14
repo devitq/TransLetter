7120ce929f424f0a8635f43f828d268795eadcaa
ITQ <itq.dev@ya.ru>
2023-12-14 20:29:31 +0300

    Black fixes

--- transletter/translator_request/views.py
+++ transletter/translator_request/views.py
@@ -94,8 +94,12 @@ class RequestTranslatorView(LoginRequiredMixin, View):
 
         if form.is_valid() and request_status in "SERJACNN":
             resume = form["resume_form"]
-            request.user.first_name = form["user_form"].cleaned_data["first_name"]
-            request.user.last_name = form["user_form"].cleaned_data["last_name"]
+            request.user.first_name = form["user_form"].cleaned_data[
+                "first_name"
+            ]
+            request.user.last_name = form["user_form"].cleaned_data[
+                "last_name"
+            ]
             request.user.save()
             files = request.FILES.getlist("files_form-file")
 
