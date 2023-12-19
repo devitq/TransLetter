848e9b9b9016ad821697c2ec30c27d7c8f638bad
ITQ <itq.dev@ya.ru>
2023-12-19 18:22:20 +0300

    Added check.sh

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+sort-requirements requirements/dev.txt
+sort-requirements requirements/prod.txt
+sort-requirements requirements/test.txt
+sort-requirements requirements/lints.txt
+printf "${GREEN}Requirements sorted${NC}\n"
+
+cd transletter
+
+black .
+flake8 .
+djlint . --reformat --format-css --format-js
+printf "${GREEN}Linters runned${NC}\n"
+
+python manage.py makemigrations --check
+python manage.py test
+printf "${GREEN}Tests runned${NC}\n"
